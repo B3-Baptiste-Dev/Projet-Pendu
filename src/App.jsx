801d567338622e5ef4ac9d7f53e90f78e9a161ce
Baptiste <34516070+BaptisteLonguepee@users.noreply.github.com>
2024-01-31 15:05:29 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import './App.css'
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Home from "./components/home/home.jsx";
+import Statistics from "./components/Statistics/Statistics.jsx";
 // import Play from "./components/play/play.jsx";
-// import Statistics from "./components/statistics/statistics.jsx";
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
               <Routes>
                   <Route path="/" element={<Home />}></Route>
                   {/*<Route path="/play" element={<Play />}></Route>*/}
-                  {/*<Route path="/statistics" element={<Statistics />} />*/}
+                  <Route path="/statistics" element={<Statistics />} />
                   <Route path="*" element={<h1>Error 404</h1>}></Route>
               </Routes>
           </BrowserRouter>
