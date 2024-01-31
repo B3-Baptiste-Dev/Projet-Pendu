49b0a5577c5faf03ed5a1718f39c4802503609c6
Baptiste <34516070+BaptisteLonguepee@users.noreply.github.com>
2024-01-31 15:39:17 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Navbar from "./components/navbar/navbar.jsx";
 import Home from "./components/home/home.jsx";
-// import Play from "./components/play/play.jsx";
+import Play from "./components/play/play.jsx";
 import Statistics from "./components/Statistics/Statistics.jsx";
 import Notfound from "./components/notfound/notfound.jsx";
 
@@ -13,7 +13,7 @@ function App() {
               <Navbar />
               <Routes>
                   <Route path="/" element={<Home />} />
-                  {/*<Route path="/play" element={<Play />}></Route>*/}
+                  <Route path="/play" element={<Play />}></Route>
                   <Route path="/statistics" element={<Statistics />} />
                   <Route path="*" element={<Notfound />} />
               </Routes>
